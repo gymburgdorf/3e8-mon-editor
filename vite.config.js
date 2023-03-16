a31fa1206ba2573a6969c7e9db4d20c0e3d4b48c
gymburgdorf <adrian.luethi@gymburgdorf.ch>
2023-03-16 20:58:08 +0100

    change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: "https://gymburgdorf.github.io/3e8-editor",
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
