e9bd07bd4f39606c6485fb8108f8ae313ab259d9
gymburgdorf <adrian.luethi@gymburgdorf.ch>
2023-03-17 09:51:43 +0100

    try relative base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: "./",
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
