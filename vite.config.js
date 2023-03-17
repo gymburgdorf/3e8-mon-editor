df61b13f6e88b81c20b01ab22f9439662087e68a
gymburgdorf <adrian.luethi@gymburgdorf.ch>
2023-03-17 08:56:02 +0100

    test npm

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "https://gymburgdorf.github.io/3e8-editor",
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
